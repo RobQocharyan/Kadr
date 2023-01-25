798a38acad5c8f232a24f0c6f35dc5a37143fed7
Arman <mnacakanovarman@mail.ru>
2023-01-25 23:36:14 +0400

    Hello

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
+import { Home } from './Component/Home_page/Home';
 import './App.scss';
+import { Route, Routes } from 'react-router';
 import { Header } from './Component/Header/Header';
 import { Footer } from './Component/Footer/Footer';
+import { Login } from './Component/Login/LoginUser/Login';
 import { Registration } from './Component/Login/Registration/Registration';
 
 function App() {
   return (
     <div className="App">
+      {/* <Home /> */}
       <Header />
-      <div></div>
+      <div>
+            <Routes>
+                      <Route>
+                          <Route path='Registration' element={<Registration />} />
+                          <Route path='Login' element={<Login />} />
+                      </Route>
+            </Routes>
+      </div>
+      
       <Footer />
+
     </div>
   );
 }
---
+++ src/Kadr_am/KadrAm.tsx
@@ -0,0 +1,18 @@
+import { FC } from 'react';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Registration } from '../Component/Login/Registration/Registration';
+import { Header } from '../Component/Header/Header';
+import { Login } from '../Component/Login/LoginUser/Login';
+import { Home } from '../Component/Home_page/Home'
+import { No_Page } from '../Component/NoPage/Nopage';
+
+
+
+// export const Kadr: FC = () => {
+//     return (
+            
+//     )
+// }
+
+
