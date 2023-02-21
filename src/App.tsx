6e7d3372b6fb764be02a80d70f1f28b94ba89a38
RobQocharyan <robqocharyan@mail.ru>
2023-02-21 21:33:48 +0400

    j

--- src/App.tsx
+++ src/App.tsx
@@ -6,30 +6,25 @@ import { Header } from './Component/Header/Header';
 import { Footer } from './Component/Footer/Footer';
 import { Login } from './Component/Login/LoginUser/Login';
 import { Registration } from './Component/Login/Registration/Registration';
-import { Route, Routes } from 'react-router';
 
 function App() {
   return (
-    <div className="App">
+    <><div className="App">
       {/* <Home /> */}
       <Header />
       <div>
 
-       <Registration />
+        <Registration />
       </div>
 
-            <Routes>
-                      <Route>
-                          <Route path='Registration' element={<Registration />} />
-                          <Route path='Login' element={<Login />} />
-                      </Route>
-            </Routes>
-      </div>
-      
+      <Routes>
+        <Route>
+          <Route path='Registration' element={<Registration />} />
+          <Route path='Login' element={<Login />} />
+        </Route>
+      </Routes>
+    </div><Footer /></>
 
-      <Footer />
-
-    </div>
   );
 }
 
